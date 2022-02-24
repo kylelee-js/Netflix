69822d1bbd7d38154dbb2f227d3de87be5ebe6ec
lee gihun <leegihun@leeui-MacBookAir-2.local>
2022-02-24 14:25:08 +0900

    laptop commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
 import React from "react";
+import styled from "styled-components";
 import "./App.css";
 
+const Circle = styled.div`
+  background-color: tomato;
+  height: 100px;
+  width: 100px;
+  border-radius: 15%;
+`;
+
 function App() {
-  return <div className="App">first link</div>;
+  return (
+    <div className="App">
+      <Circle>tomato</Circle>
+    </div>
+  );
 }
 
 export default App;
